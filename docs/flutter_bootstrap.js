677ffc47d307dedf7758d5c6459a4b8edc830c3d
AbdoEmad11 <Abdelrahmanemad581@gmail.com>
2025-09-10 20:47:57 +0200

    Add View Details and GitHub buttons to project cards in home page

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "324775149"
+    serviceWorkerVersion: "2167425084"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -39,14 +39,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "aa2146e126d37c164c52a4efe63b78b1
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "96851250bf9c9ef3216aeda2fcf5739a",
+"flutter_bootstrap.js": "1efcf5571a6beda31a3f6290328cb168",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c46ca0aa445bdfb36e41394644a9cac7",
 "/": "c46ca0aa445bdfb36e41394644a9cac7",
-"main.dart.js": "5e84bb95160fc42f43936c02cf40ca4e",
+"main.dart.js": "917d75eee1e54b60b32ad70f6277abab",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "fb8d4678888024cd5f83d8522bb3a722"};
 // The application shell files that are downloaded before a service worker can
